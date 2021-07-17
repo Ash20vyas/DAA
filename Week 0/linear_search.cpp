953dd4e5dd5be7aa5a1240a5398c54b5ce8dd97a
Ashutosh Vyas <54895033+Ash20vyas@users.noreply.github.com>
2021-07-17 19:30:17 +0530

    commit on 'main'

--- Week 0/linear_search.cpp
+++ Week 0/linear_search.cpp
@@ -2,6 +2,33 @@
 using namespace std;
 int main()
 {
-    cout << "Hello" << endl;
+    int n,t,x,count,flag;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cin >> t;
+    while (t--)
+    {
+        cin >> x;
+        count = flag = 0;
+        for (int i = 0; i < n; i++)
+        {
+            count++;
+            if(x == arr[i])
+            {
+                flag = 1;
+                break;
+            }
+        }
+        if(flag)
+            cout << "Found after " << count << " comparisons" << endl;
+        else
+            cout << "Not Found" << endl;
+    }
+    
+    
     return 0;
 }
