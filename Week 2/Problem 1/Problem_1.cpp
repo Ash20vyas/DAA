8fc06c202faa5f738234a8639ac1145fb877e667
Ashutosh Vyas <54895033+Ash20vyas@users.noreply.github.com>
2021-07-29 14:01:18 +0530

    Week 2 completed

--- Week 2/Problem 1/Problem_1.cpp
+++ Week 2/Problem 1/Problem_1.cpp
@@ -16,20 +16,38 @@ int main()
         {
             in >> arr[i];
         }
-        int key, count = 0;
+        int key, count = 0, li = -1, ui = -1;
         in >> key;
         int l = 0, u = n, m = (l + u) / 2;
+        // while (l < u)
+        // {
+        //     if (arr[m] == key)
+        //     {
+        //         count++;
+        //         for (int i = m + 1; i < n && arr[i] == key; i++)
+        //             count++;
+        //         for (int i = m - 1; i >= 0 && arr[i] == key; i--)
+        //             count++;
+        //         out << key << " - " << count << endl;
+        //         goto outer;
+        //     }
+        //     else if (arr[m] < key)
+        //         l = m + 1;
+        //     else
+        //         u = m;
+        //     m = (l + u) / 2;
+        // }
         while (l < u)
         {
             if (arr[m] == key)
             {
-                count++;
-                for (int i = m + 1; i < n && arr[i] == key; i++)
-                    count++;
-                for (int i = m - 1; i >= 0 && arr[i] == key; i--)
-                    count++;
-                out << key << " - " << count << endl;
-                goto outer;
+                if (m == 0 || arr[m - 1] != key)
+                {
+                    li = m;
+                    break;
+                }
+                else
+                    u = m;
             }
             else if (arr[m] < key)
                 l = m + 1;
@@ -37,7 +55,31 @@ int main()
                 u = m;
             m = (l + u) / 2;
         }
-        out << "Key not present" << endl;
+        if (li >= 0)
+        {
+            l = 0, u = n, m = (l + u) / 2;
+            while (l < u)
+            {
+                if (arr[m] == key)
+                {
+                    if (m == n - 1 || arr[m + 1] != key)
+                    {
+                        ui = m;
+                        break;
+                    }
+                    else
+                        l = m + 1;
+                }
+                else if (arr[m] < key)
+                    l = m + 1;
+                else
+                    u = m;
+                m = (l + u) / 2;
+            }
+            out << key << " - " << (ui - li + 1) << endl;
+        }
+        else
+            out << "Key not present" << endl;
     outer:
         continue;
     }
